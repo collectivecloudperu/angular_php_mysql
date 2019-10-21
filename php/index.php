30dc3901c60693d807f845f46bcd0b783043399a
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2019-10-21 11:25:09 -0500

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new mysqli("localhost", "usuario", "pass", "db"); 
+$conn = new mysqli("localhost", "usuario", "password", "basededatos"); 
 
 $conn->query("SET NAMES 'utf8'");
 
